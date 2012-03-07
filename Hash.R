5a20f147750c1be1364b95764d7f86c6003c8384
Sam Swift <samswift@gmail.com>
2012-03-06 20:05:46 -0500

    improved hashing utilities which reduce the number of mistakes and format incompatibilities

--- Hash.R
+++ Hash.R
@@ -2,11 +2,20 @@ require(digest)
 
 hashVect <- function(vector) {
   vector <- lapply(vector,function(x){
-    if(!is.na(x)){
-      digest(x)
-    } else {
+    if(is.na(x) | nchar(x)==0){
       NA
+    } else {
+      digest(x)
     }
   })
   unlist(vector)
 }
+
+hashAndSave <- function(inFname, outFname, cols){
+  df <- read.csv(inFname, stringsAsFactors = F)
+  for(i in 1:length(cols)){
+    df[,cols[i]] <- hashVect(df[,cols[i]])
+  }
+  write.csv(df,outFname,row.names=F)
+}
+
