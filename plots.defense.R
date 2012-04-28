3f7023bc1bda54325ff4939d55d4333beec66388
Sam Swift <samswift@gmail.com>
2012-04-28 11:25:20 -0400

    labels and factor orders on three main ABS2 plots

--- plots.defense.R
+++ plots.defense.R
@@ -17,15 +17,16 @@ dodge   <- position_dodge(width=0.9)
 ##############################
 source("ABS2 Analysis.R")
 
+ABS2$ArgCond <- factor(ABS2$ArgCond, levels=c("NoArg","Arg"), labels=c("No Argument","Argument"))
+
 sv5_sum <- barVals(ABS2, c("RoleCond", "ArgCond"),"sv5")
 ggplot(data=sv5_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) + 
   geom_bar(position="dodge", stat="identity") + 
   geom_errorbar(limits, position=dodge, width=0.1, size=.75, col="white") +
   xlab("Argument Condition") +
   ylab("Subjective Value (5-item, Z-score)") +
-  scale_fill_manual(values = rhg_cols1) +
+  scale_fill_manual(values = rhg_cols1, name="Role") +
   theme_black_presentation()
-ggsave()
 
 rp_sum <- barVals(ABS2, c("RoleCond", "ArgCond"),"RP")
 ggplot(data=rp_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) + 
@@ -35,8 +36,15 @@ ggplot(data=rp_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) +
   scale_y_continuous(breaks=seq(2000,2500,100)) +
   xlab("Argument Condition") +
   ylab("Reservation Price ($)") +
-  scale_fill_manual(values = rhg_cols1) +
+  scale_fill_manual(values = rhg_cols1, name="Role") +
   theme_black_presentation()
 
-
+resreact_sum <- barVals(ABS2, c("RoleCond", "ArgCond"),"ResReact")
+ggplot(data=resreact_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) + 
+  geom_bar(position="dodge", stat="identity") + 
+  geom_errorbar(limits, position=dodge, width=0.1, size=.75, col="white") +
+  xlab("Argument Condition") +
+  ylab("Reaction to Response (3-item, Z-score)") +
+  scale_fill_manual(values = rhg_cols1, name="Role") +
+  theme_black_presentation()
 
