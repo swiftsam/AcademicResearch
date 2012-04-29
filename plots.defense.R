eae9f46d13a4a37608e3a123ac4183661d647057
Sam Swift <samswift@gmail.com>
2012-04-29 19:40:00 -0400

    maybe grey is nicer

--- plots.defense.R
+++ plots.defense.R
@@ -20,7 +20,7 @@ source("JNM Analysis.R")
 
 jnm.sat_sum <- barVals(jnm, c("Negotiate"),"Satisfaction")
 ggplot(data=jnm.sat_sum, aes(fill=Negotiate, y=mean, x=Negotiate)) + 
-  geom_bar(position="dodge", stat="identity",color="white") + 
+  geom_bar(position="dodge", stat="identity",color="grey70") + 
   geom_errorbar(limits, position=dodge, width=0.1, size=.75, col="white") +
   scale_y_continuous(limits=c(1,7),oob=rescale_none) +
   xlab("Choice to Negotiate") +
@@ -46,7 +46,7 @@ ABS2$ArgCond <- factor(ABS2$ArgCond, levels=c("NoArg","Arg"), labels=c("No Argum
 
 sv5_sum <- barVals(ABS2, c("RoleCond", "ArgCond"),"sv5")
 ggplot(data=sv5_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) + 
-  geom_bar(position="dodge", stat="identity",color="white") + 
+  geom_bar(position="dodge", stat="identity",color="grey70") + 
   geom_errorbar(limits, position=dodge, width=0.1, size=.75, col="white") +
   xlab("Argument Condition") +
   ylab("Subjective Value (5-item, Z-score)") +
@@ -55,7 +55,7 @@ ggplot(data=sv5_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) +
 
 rp_sum <- barVals(ABS2, c("RoleCond", "ArgCond"),"RP")
 ggplot(data=rp_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) + 
-  geom_bar(position="dodge", stat="identity",color="white") +
+  geom_bar(position="dodge", stat="identity",color="grey70") +
   geom_errorbar(limits, position=dodge, width=0.1,size=.75, col="white") +
   scale_y_continuous(limits=c(2000,2500),oob=rescale_none) +
   xlab("Argument Condition") +
@@ -65,7 +65,7 @@ ggplot(data=rp_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) +
 
 resreact_sum <- barVals(ABS2, c("RoleCond", "ArgCond"),"ResReact")
 ggplot(data=resreact_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) + 
-  geom_bar(position="dodge", stat="identity",color="white") + 
+  geom_bar(position="dodge", stat="identity",color="grey70") + 
   geom_errorbar(limits, position=dodge, width=0.1, size=.75, col="white") +
   xlab("Argument Condition") +
   ylab("Reaction to Response (3-item, Z-score)") +
