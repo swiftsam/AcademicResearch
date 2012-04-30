4c9efa851e92c0a150c473fd9828813f9876189f
Sam Swift <samswift@gmail.com>
2012-04-30 18:33:43 -0400

    CSC plot and colorBrewer colors

--- plots.defense.R
+++ plots.defense.R
@@ -1,9 +1,11 @@
 library(sciplot)
 library(ggplot2)
+library(plyr)
 library(grid)
 library(scales)
+library(RColorBrewer)
 source("ggplot_themes.R")
-rhg_cols1<- c("#006699","#AA1111","#7CAE00","#00BFC4")
+palette<- brewer.pal(6,name="Set1")
 
 barVals <- function(df,factors,var){
   ddply(df, factors, function(df)
@@ -25,17 +27,33 @@ ggplot(data=jnm.sat_sum, aes(fill=Negotiate, y=mean, x=Negotiate)) +
   scale_y_continuous(limits=c(1,7),oob=rescale_none) +
   xlab("Choice to Negotiate") +
   ylab("Satisfaction with Offer") +
-  scale_fill_manual(values = rhg_cols1, name="Negotiate") +
+  scale_fill_manual(values = palette, name="Negotiate") +
   opts(legend.position = "none") +
   theme_black_presentation()
 
-rm(list=ls())
 ##############################
 #Study 2, CSC
 ##############################
 source("CSC Analysis.R")
-bargraph.CI(ArgCond,GlobSatZ,ylab="Satisfaction", xlab="Condition",main="Study 2: Satisfaction by Argument Condition")
-rm(list=ls())
+
+csc$ArgCond <- factor(csc$ArgCond, 
+                      levels=c("None","Free","OwnLow","OtherHigh","OwnBatna","OtherBatna"), 
+                      labels=c("No Argument","Uncoached\nArgument","Own\nLow Value","Other\nHigh Value","Own\nBATNA Strong","Other\nBATNA Weak"))
+csc.sat_sum <- barVals(csc,c("ArgCond"),"GlobSatZ")
+
+ggplot(data=csc.sat_sum, aes(y=mean, x=ArgCond,fill=ArgCond)) +
+  geom_bar(position="dodge", stat="identity",color="grey70") + 
+  geom_errorbar(limits, position=dodge, width=0.1, size=.75, col="white") +
+  xlab("Argument Condition") +
+  ylab("Satisfaction (5 item, Z-score)") +
+  scale_fill_manual(values = brewer.pal(6,name="Paired"), name="Condition") +
+  opts(legend.position = "none") +
+  theme_black_presentation()
+
+
+##############################
+#Study 3, SUN
+##############################
 
 ##############################
 #Study 4, ABS2
@@ -50,7 +68,7 @@ ggplot(data=sv5_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) +
   geom_errorbar(limits, position=dodge, width=0.1, size=.75, col="white") +
   xlab("Argument Condition") +
   ylab("Subjective Value (5-item, Z-score)") +
-  scale_fill_manual(values = rhg_cols1, name="Role") +
+  scale_fill_manual(values = palette, name="Role") +
   theme_black_presentation()
 
 rp_sum <- barVals(ABS2, c("RoleCond", "ArgCond"),"RP")
@@ -60,7 +78,7 @@ ggplot(data=rp_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) +
   scale_y_continuous(limits=c(2000,2500),oob=rescale_none) +
   xlab("Argument Condition") +
   ylab("Reservation Price ($)") +
-  scale_fill_manual(values = rhg_cols1, name="Role") +
+  scale_fill_manual(values = palette, name="Role") +
   theme_black_presentation()
 
 resreact_sum <- barVals(ABS2, c("RoleCond", "ArgCond"),"ResReact")
@@ -69,12 +87,12 @@ ggplot(data=resreact_sum, aes(fill=RoleCond, y=mean, x=ArgCond)) +
   geom_errorbar(limits, position=dodge, width=0.1, size=.75, col="white") +
   xlab("Argument Condition") +
   ylab("Reaction to Response (3-item, Z-score)") +
-  scale_fill_manual(values = rhg_cols1, name="Role") +
+  scale_fill_manual(values = palette, name="Role") +
   theme_black_presentation()
 
 ggplot(wfDiff.BS.Arg, aes(reorder(factor(word),relFreqDiff),relFreqDiff)) + 
-  geom_bar(aes(fill=as.numeric(rating)),color="white") + 
-  scale_fill_continuous(limits=c(-1.75, 1.75), low="red",high="green",name="Rated Value") +
+  geom_bar(aes(fill=as.numeric(rating),10)) + 
+  scale_fill_gradient2(low="#A50026",mid="#FFFFBF",high="#006837", name="Rated Value", limits=c(-1.5,1.5)) +
   coord_flip() +
   scale_y_continuous() +
   xlab("Words") +
@@ -83,8 +101,8 @@ ggplot(wfDiff.BS.Arg, aes(reorder(factor(word),relFreqDiff),relFreqDiff)) +
   theme_black_presentation()
 
 ggplot(wfDiff.BS.Sal, aes(reorder(factor(word),relFreqDiff),relFreqDiff)) + 
-  geom_bar(aes(fill=as.numeric(rating)),color="white") +
-  scale_fill_continuous(limits=c(-1.75, 1.75), low="red",high="green",name="Rated Value") +
+  geom_bar(aes(fill=as.numeric(rating))) +
+  scale_fill_gradient2(low="#A50026",mid="#FFFFBF",high="#006837", name="Rated Value", limits=c(-1.5,1.5)) +
   coord_flip() +
   scale_y_continuous() +
   xlab("Words") +
